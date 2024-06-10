2533b3581aa947e57847c3c22fb613d4c9343180
shaheer <syedarbaz3684@gmail.com>
2024-06-10 10:37:39 -0700

    backend chnges

--- resources/views/Packages.blade.php
+++ resources/views/Packages.blade.php
@@ -6,9 +6,10 @@
     <!-- Begin Page Content -->
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 heading">
+            <div class="col-lg-6 heading mb-2">
                 <!-- Page Heading -->
                 <h2 class="h3 mb-2 text-gray-800">Packages</h2>
+                <p>Add and Edit Packages Here.</p>
             </div>
         </div>
 
